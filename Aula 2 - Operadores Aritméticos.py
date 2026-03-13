5747a8402c43ae84c4fb441d42df15ba2fda3d67
Bianca Francis <kadu_300@live.com>
2026-03-13 20:00:45 -0300

    Update Aula 2 - Operadores Aritméticos.py
    Correção

--- Aula 2 - Operadores Aritméticos.py
+++ Aula 2 - Operadores Aritméticos.py
@@ -2,5 +2,34 @@ numero_1 = 5
 numero_2 = 2
 
 soma = numero_1 + numero_2
+# retorno: 7
 
-print(soma)
+subtracao = numero_1 - numero_2
+#retorno: 3
+
+multiplicacao = numero_1 * numero_2
+#retorno: 10
+
+divisao = numero_1 / numero_2
+#retorno: 2.5
+
+#Operações de divisão sempre retornam um número do tipo FLOAT, mesmo que o resultado seja um número inteiro. 
+
+#Para obter um resultado inteiro, podemos usar a divisão inteira, com duas barras (//):
+
+divisao_inteira = numero_1 // numero_2
+#retorno: 2  
+
+#O operador de módulo (%) retorna o resto da divisão entre dois números:
+modulo = numero_1 % numero_2
+#retorno: 1 
+
+#O operador de exponenciação (**) é usado para elevar um número a uma potência:
+exponenciacao = numero_1 ** numero_2
+#retorno: 25
+
+"ola" + "mundo"
+#retorno: "olamundo"
+
+"ola" * 3
+#retorno: "olaolaola"  
